b89dab55c174cffe6d07fcd49ae48a850844e2e1
icuzmici <intel66666666@gmail.com>
2021-03-24 20:25:04 +0200

    pending...

--- AppComponents.js
+++ AppComponents.js
@@ -1,6 +1,6 @@
 import { AppStorage } from "./AppStorage.js";
 
-export function displayAppName() {
+function displayAppName() {
   const fragment = document.createDocumentFragment();
   const name = document.createElement("h1");
 
@@ -12,8 +12,12 @@ export function displayAppName() {
   return fragment;
 }
 
-export class Form {
-  static createForm() {
+class Form extends AppStorage {
+  constructor() {
+    super("form");
+  }
+
+  createForm() {
     const formFragmentBox = document.createDocumentFragment();
     const formFragmentsComponents = document.createDocumentFragment();
     const hr1 = document.createElement("hr");
@@ -40,13 +44,20 @@ export class Form {
 
     formSection.addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log(checkbox)
+      this.generateArticleId();
+      if (Articles.storeArticle(this.state)) {
+        updateArticlesFeed(this.state.visible ? 'visible' : 'hidden')
+        toggleFeedTitle(this.state.visible)
+        
+        this.state = ""; 
+        formSection.reset();
+      }
     });
 
     return formFragmentBox;
   }
 
-  static createTitleInput() {
+  createTitleInput() {
     const titleInput = document.createElement("input");
 
     titleInput.type = "text";
@@ -58,19 +69,14 @@ export class Form {
     titleInput.minLength = "20";
     titleInput.maxLength = "180";
 
-    const titleStorageValue = new AppStorage(titleInput.name);
+    this.storageInputsValues(titleInput);
 
-    titleInput.value = titleStorageValue.state;
-
-    titleInput.addEventListener("input", (e) => {
-      titleStorageValue.state = titleInput.value;
-      console.log(titleInput.value);
-    });
+    titleInput.value = this.state.title || "";
 
     return titleInput;
   }
 
-  static createContentInput() {
+  createContentInput() {
     const contentInput = document.createElement("textarea");
 
     contentInput.id = "form-content";
@@ -81,29 +87,24 @@ export class Form {
     contentInput.maxLength = "1080";
     contentInput.cols = "100";
     contentInput.rows = "9";
+    contentInput.value = this.state.content || "";
 
-    const contentStorageValue = new AppStorage(contentInput.name);
-
-    contentInput.value = contentStorageValue.state;
-
-    contentInput.addEventListener("input", (e) => {
-      contentStorageValue.state = contentInput.value;
-      console.log(contentInput.value);
-    });
+    this.storageInputsValues(contentInput);
 
     return contentInput;
   }
 
-  static createCheckBoxArea() {
+  createCheckBoxArea() {
     const checkBoxContainer = document.createElement("div");
 
     checkBoxContainer.id = "checkbox-area";
 
-    const checkBoxRadio = document.createElement("input");
+    this.checkBoxRadio = document.createElement("input");
 
-    checkBoxRadio.type = "checkbox";
-    checkBoxRadio.name = "visible";
-    checkBoxRadio.id = "visible";
+    this.checkBoxRadio.type = "checkbox";
+    this.checkBoxRadio.name = "visible";
+    this.checkBoxRadio.id = "visible";
+    this.checkBoxRadio.checked = this.state.visible;
 
     const checkBoxLabel = document.createElement("label");
 
@@ -114,13 +115,23 @@ export class Form {
     checkBoxBtnName.innerText = "Visible";
 
     checkBoxLabel.appendChild(checkBoxBtnName);
-    checkBoxContainer.appendChild(checkBoxRadio);
+    checkBoxContainer.appendChild(this.checkBoxRadio);
     checkBoxContainer.appendChild(checkBoxLabel);
+
+    checkBoxContainer.addEventListener(
+      "input",
+      ({ target: { checked, name } }) => {
+        this.state = {
+          ...this.state,
+          [name]: checked,
+        };
+      }
+    );
 
     return checkBoxContainer;
   }
 
-  static createSubmit() {
+  createSubmit() {
     const submitBtn = document.createElement("input");
 
     submitBtn.type = "submit";
@@ -129,8 +140,249 @@ export class Form {
 
     return submitBtn;
   }
+
+  generateArticleId() {
+    let date = Date.now();
+    this.state = {
+      ...this.state,
+      articleId: date,
+    };
+  }
+
+  storageInputsValues(element) {
+    element.addEventListener("input", ({ target: { value, name } }) => {
+      this.state = {
+        ...this.state,
+        [name]: value,
+      };
+    });
+  }
+
+}
+
+class Articles extends AppStorage {
+  constructor() {
+    super("articles");
+    this.state = {
+      visibleArticles: this.state.visibleArticles || [],
+      hiddenArticles: this.state.hiddenArticles || [],
+    };
+  }
+
+  createArticleFeedTitleAndBtns() {
+    const container = document.createDocumentFragment();
+    const title = document.createElement("h2");
+    const btnContainer = document.createElement("div");
+    const showVisibleBtn = document.createElement("button");
+    const showHiddenBtn = document.createElement("button");
+    const visibleBtnText = document.createElement("h4");
+    const hiddenBtnText = document.createElement("h4");
+
+    title.innerText =  'List of Visible Articles';
+    title.id = "list-title";
+    btnContainer.id = "filter-articles";
+    showVisibleBtn.id = "visible-articles";
+    showHiddenBtn.id = "hidden-articles";
+    visibleBtnText.innerText = "Visible Articles";
+    hiddenBtnText.innerText = "Hidden Articles";
+
+    showHiddenBtn.appendChild(hiddenBtnText);
+    showVisibleBtn.appendChild(visibleBtnText);
+    btnContainer.appendChild(showVisibleBtn);
+    btnContainer.appendChild(showHiddenBtn);
+    container.appendChild(title);
+    container.appendChild(btnContainer);
+
+
+    showVisibleBtn.addEventListener('click',()=>{
+      updateArticlesFeed('visible')
+      title.innerText = 'List of Visible Articles'
+    })
+
+    showHiddenBtn.addEventListener('click',()=>{
+      title.innerText = 'List of Hidden Articles'
+      updateArticlesFeed('hidden')
+    })
+
+    return container;
+  }
+
+  createUlBody(type) {
+    const {visibleArticles,hiddenArticles} = this.state;
+    const unorderedList = document.createElement("ul");
+    const articlesFeed = document.createDocumentFragment();
+
+    if(type === 'visible'){
+      for(let article of visibleArticles){
+        articlesFeed.appendChild(this.createArticle(article))
+      }
+    }
+
+    else if(type === 'hidden'){
+      for(let article of hiddenArticles){
+        articlesFeed.appendChild(this.createArticle(article))
+      }
+    }
+
+    unorderedList.id = "articles-list";
+    unorderedList.appendChild(articlesFeed)
+
+    return unorderedList;
+  }
+
+  createArticle(article) {
+    const li = document.createElement('li');
+    const articleBox = document.createElement('div');
+    const articleTitle = document.createElement('div');
+    const articleTitleText = document.createElement('h3');
+    const articleContent = document.createElement('div');
+    const articleActiveBtnsBox = document.createElement('div');
+    const editBtn = document.createElement('button');
+    const editBtnIcon = document.createElement('i')
+    const deleteBtn = document.createElement('button');
+    const deleteBtnIcon = document.createElement('i');
+    const changeVsbltyBtn = document.createElement('button');
+    const changeVsbltyBtnIcon = document.createElement('i')
+    const saveEdittingBtn = document.createElement('button');
+    const saveEdittingBtnIcon = document.createElement('i')
+
+    li.id = article.articleId
+    articleBox.className = 'article-box'
+    articleTitle.className = 'article-title';
+    articleTitleText.innerText = article.title;
+    articleContent.className = 'article-content';
+    articleContent.innerText = article.content
+    articleActiveBtnsBox.className = 'active-btns';
+    editBtn.className = 'edit-btn';
+    editBtnIcon.className = 'far fa-edit';
+    deleteBtn.classList = 'delete-btn';
+    deleteBtnIcon.classList = 'fas fa-trash';
+    changeVsbltyBtn.className = article.visible ? 'change-vsbl-btn show' : 'change-vsbl-btn hide';
+    changeVsbltyBtnIcon.className = article.visible ? 'far fa-eye' : 'far fa-eye-slash';
+    saveEdittingBtn.className ='save-edit';
+    saveEdittingBtn.disabled = true
+    saveEdittingBtnIcon.className='far fa-save';
+
+    saveEdittingBtn.appendChild(saveEdittingBtnIcon);
+    changeVsbltyBtn.appendChild(changeVsbltyBtnIcon);
+    deleteBtn.appendChild(deleteBtnIcon);
+    editBtn.appendChild(editBtnIcon);
+    articleActiveBtnsBox.appendChild(editBtn)
+    articleActiveBtnsBox.appendChild(deleteBtn)
+    articleActiveBtnsBox.appendChild(changeVsbltyBtn)
+    articleActiveBtnsBox.appendChild(saveEdittingBtn);
+    articleTitle.appendChild(articleTitleText);
+    articleBox.appendChild(articleTitle);
+    articleBox.appendChild(articleContent);
+    articleBox.appendChild(articleActiveBtnsBox);
+    li.appendChild(articleBox);
+
+    deleteBtn.addEventListener('click',()=>{
+      this.deleteArticle(li)
+    })
+
+    return li;
+  }
+
+  createArticleFeedBody() {
+    const fragment = document.createDocumentFragment();
+    const ulBody = this.createUlBody('visible');
+
+    fragment.appendChild(this.createArticleFeedTitleAndBtns());
+    fragment.appendChild(ulBody)
+  
+    return fragment;
+  }
+
+
+ static storeArticle(article) {
+    const { visibleArticles, hiddenArticles } = JSON.parse(localStorage.getItem('articles'));
+    const massiveArray = visibleArticles.concat(hiddenArticles);
+    const filteredArticlesTitle = massiveArray.filter(
+      (art) =>
+        art.title.toLowerCase().trim() === article.title.toLowerCase().trim()
+    ).length;
+
+    if (article.visible && !filteredArticlesTitle) {
+      visibleArticles.unshift(article);
+    } 
+    
+    else if (!article.visible && !filteredArticlesTitle) {
+      hiddenArticles.unshift(article);
+    }
+    
+    else {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: `Error!`,
+        text: `An article with such title already exists, please modify it.`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return false;
+    }
+
+
+    Swal.fire({
+      icon: "success",
+      title: `Success`,
+      message: "An article has been successfully created!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+   localStorage.setItem('articles', JSON.stringify({visibleArticles,hiddenArticles}))
+    return true;
+  }
+
+  deleteArticle(deletedArticle){
+    const feed = document.getElementById('articles-list')
+    const {visibleArticles,hiddenArticles} = this.state
+    const newVisibleArticles = visibleArticles.filter(art=>art.articleId != deletedArticle.id)
+    const newHiddenArticles = hiddenArticles.filter(art=>art.articleId != deletedArticle.id)
+    if(newVisibleArticles.length !== visibleArticles.length){
+     this.state = {
+       ...this.state,
+       visibleArticles : newVisibleArticles
+     }
+    }
+    else if(newHiddenArticles.length !== hiddenArticles.length){
+      this.state = {
+        ...this.state,
+        hiddenArticles : newHiddenArticles
+      }
+    }
+    feed.removeChild(deletedArticle)
+  }
+}
+
+export function displayAppComponents() {
+  const name = displayAppName();
+  const form = new Form();
+  const articles = new Articles();
+
+  document.body.appendChild(name);
+  document.body.appendChild(form.createForm());
+  document.body.appendChild(articles.createArticleFeedBody());
 }
 
 
+function updateArticlesFeed(type){
+  const oldFeed = document.getElementById('articles-list')
+  const updatedFeed = new Articles();
+
+  document.body.removeChild(oldFeed)
+  document.body.appendChild(updatedFeed.createUlBody(type))
+}
+
+function toggleFeedTitle(type){
+const title = document.getElementById('list-title');
+if(type){
+  title.innerText = 'List of Visible Articles'
+}
+else if(!type){
+  title.innerText = 'List of Hidden Articles'
+}
+}
 
 
--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// const ul = document.createElement('ul')
-// let fragment = document.createDocumentFragment();
-// let browsers = ['Firefox', 'Chrome', 'Opera',
-//     'Safari', 'Internet Explorer'];
+import { displayAppComponents } from "./AppComponents.js";
 
-// browsers.forEach(function(browser) {
-//     let li = document.createElement('li');
-//     li.textContent = browser;
-//     fragment.appendChild(li);
-// });
-// ul.appendChild(fragment);
-// document.body.appendChild(ul)
-import {displayAppName,Form} from './AppComponents.js'
-import { AppStorage } from "./AppStorage.js";
-
-const appComponents = [displayAppName(),Form.createForm()]
-const fragment = document.createDocumentFragment();
-appComponents.forEach(com=>{
-    fragment.appendChild(com)
-})
-
-
-
-
-document.body.appendChild(fragment);
+displayAppComponents()
